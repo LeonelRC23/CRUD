3ca753d0eba06af47a647dd8b38b60188e54d78d
Cordero Leonel Rodrigo <leonelrcordero22@gmail.com>
2023-11-29 10:25:05 -0300

    Creamos un objeto que guardamos en la agenda que esta almacenada en localstorage

--- JS/app.js
+++ JS/app.js
@@ -4,11 +4,28 @@ import Contacto from "./classContacto.js";
 // const contactoNuevo = new Contacto(1, "algun nombre", "apellido", "sadsad@gsdsdf.com", 4528039);
 const agenda = [];
 const formularioContacto = document.querySelector(`form`);
+const nombre = document.querySelector(`#nombre`),
+apellido = document.querySelector(`#apellido`),
+email = document.querySelector(`#email`),
+telefono = document.querySelector(`#telefono`);
+
 
 
 //funciones
-const crearContacto = () => {
+const crearContacto = (e) => {
+    e.preventDefault();
+    console.log(`Desde la funcion crear contacto`);
+    //validar datos
+    //crear el objeto con los datos del formulario
+    const contactoNuevo = new Contacto(1, nombre.value, apellido.value, email.value, telefono.value);
+    //guardar el objeto en un array agenda
+    agenda.push(contactoNuevo);
+    guardarEnLocalStorage();
+    console.log(agenda);
+};
 
+const guardarEnLocalStorage = () => {
+    localStorage.setItem(`agendaKey`, JSON.stringify(agenda));
 }
 
 //logica
