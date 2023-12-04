dac5c6fb0260f14a730f41209f492fef77192e33
Cordero Leonel Rodrigo <leonelrcordero22@gmail.com>
2023-12-04 17:37:28 -0300

    Agregamos una ventana para consultar al usuario si esta seguro de borrar el contacto

--- JS/app.js
+++ JS/app.js
@@ -5,45 +5,49 @@ import Contacto from "./classContacto.js";
 const agenda = JSON.parse(localStorage.getItem(`agendaKey`)) || [];
 const formularioContacto = document.querySelector(`form`);
 const nombre = document.querySelector(`#nombre`),
-apellido = document.querySelector(`#apellido`),
-email = document.querySelector(`#email`),
-telefono = document.querySelector(`#telefono`);
-
-
+  apellido = document.querySelector(`#apellido`),
+  email = document.querySelector(`#email`),
+  telefono = document.querySelector(`#telefono`);
 
 //funciones
 const crearContacto = (e) => {
-    e.preventDefault();
-    console.log(`Desde la funcion crear contacto`);
-    //validar datos
-    //crear el objeto con los datos del formulario
-    const contactoNuevo = new Contacto(crypto.randomUUID(), nombre.value, apellido.value, email.value, telefono.value);
-    //guardar el objeto en un array agenda
-    agenda.push(contactoNuevo);
-    guardarEnLocalStorage();
-    console.log(agenda);
-    limpiarFormulario();
-    //dibujar fila
-    crearFila(contactoNuevo, agenda.length);
+  e.preventDefault();
+  console.log(`Desde la funcion crear contacto`);
+  //validar datos
+  //crear el objeto con los datos del formulario
+  const contactoNuevo = new Contacto(
+    crypto.randomUUID(),
+    nombre.value,
+    apellido.value,
+    email.value,
+    telefono.value
+  );
+  //guardar el objeto en un array agenda
+  agenda.push(contactoNuevo);
+  guardarEnLocalStorage();
+  console.log(agenda);
+  limpiarFormulario();
+  //dibujar fila
+  crearFila(contactoNuevo, agenda.length);
 };
 
 const guardarEnLocalStorage = () => {
-    localStorage.setItem(`agendaKey`, JSON.stringify(agenda));
-}
+  localStorage.setItem(`agendaKey`, JSON.stringify(agenda));
+};
 
-const limpiarFormulario = () =>{
-    formularioContacto.reset();
-}
+const limpiarFormulario = () => {
+  formularioContacto.reset();
+};
 
 const cargaInicial = () => {
-    if(agenda.length > 0){
-        agenda.map((contacto, posicion) => crearFila(contacto, posicion + 1));
-    }
-}
+  if (agenda.length > 0) {
+    agenda.map((contacto, posicion) => crearFila(contacto, posicion + 1));
+  }
+};
 
 const crearFila = (contacto, fila) => {
-    const tablaContacto = document.querySelector(`#tablaContacto`);
-    tablaContacto.innerHTML += `<tr>
+  const tablaContacto = document.querySelector(`#tablaContacto`);
+  tablaContacto.innerHTML += `<tr>
     <th scope="row">${fila}</th>
     <td>${contacto.nombre}</td>
     <td>${contacto.apellido}</td>
@@ -54,22 +58,46 @@ const crearFila = (contacto, fila) => {
       <button class="btn btn-warning">Editar</button>
       <button class="btn btn-danger" onclick="borrarContacto('${contacto.id}')">Borrar</button>
     </td>
-  </tr>`
-}
+  </tr>`;
+};
 
 window.borrarContacto = (idContacto) => {
-    //buscar contacto por id y obtener su posicion. findIndex
-    const posicionContactoBuscado = agenda.findIndex((contacto) => contacto.id === idContacto)
-    console.log(posicionContactoBuscado);
-    //borrar del array - splice(posicion del elemento, cuantos quiero borrar)
-    agenda.splice(posicionContactoBuscado, 1);
-    //actualizar localstoraje
-    guardarEnLocalStorage();
-    //borrar la fila de la tabla
-    const tablaContacto = document.querySelector(`tbody`);
-    console.log(tablaContacto.children[posicionContactoBuscado]);
-    tablaContacto.removeChild(tablaContacto.children[posicionContactoBuscado]);
-}
+  Swal.fire({
+    title: "¿Estas seguro de borrar el contacto?",
+    text: "No puedes revertir este paso posteriormente",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Borrar",
+    cancelButtonText: "Cancelar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // agregar la logica para borrar el contacto
+      //buscar contacto por id y obtener su posicion. findIndex
+      const posicionContactoBuscado = agenda.findIndex(
+        (contacto) => contacto.id === idContacto
+      );
+      console.log(posicionContactoBuscado);
+      //borrar del array - splice(posicion del elemento, cuantos quiero borrar)
+      agenda.splice(posicionContactoBuscado, 1);
+      //actualizar localstoraje
+      guardarEnLocalStorage();
+      //borrar la fila de la tabla
+      const tablaContacto = document.querySelector(`tbody`);
+      console.log(tablaContacto.children[posicionContactoBuscado]);
+      tablaContacto.removeChild(
+        tablaContacto.children[posicionContactoBuscado]
+      );
+      //actualizar todos los td de la tabla con el numero de fila correcto
+      Swal.fire({
+        title: "Contactato eliminado",
+        text: "Contacto eliminado exitosamente",
+        icon: "success",
+      });
+    }
+  });
+};
 
 //logica
 formularioContacto.addEventListener(`submit`, crearContacto);
